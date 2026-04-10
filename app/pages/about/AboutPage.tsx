f2d8ce1580d14f46724587bee00b4177692c2ae7
sakshi <isakshy18@gmail.com>
2026-04-10 19:58:26 +0530

    feat(about-page): compose sections and render WhyChooseUs with gallery below

--- app/pages/about/AboutPage.tsx
+++ app/pages/about/AboutPage.tsx
@@ -1,16 +1,27 @@
 import { SiteFooter } from "@/components/layout/SiteFooter";
 import { SiteHeader } from "@/components/layout/SiteHeader";
+import { WhyChooseUs } from "@/components/ui/WhyChooseUs";
+import { AboutDoctorsSection } from "./sections/AboutDoctorsSection";
+import { AboutFounderSection } from "./sections/AboutFounderSection";
+import { AboutGallerySection } from "./sections/AboutGallerySection";
+import { AboutHeroSection } from "./sections/AboutHeroSection";
+import { AboutStorySection } from "./sections/AboutStorySection";
 
 export function AboutPage() {
   return (
     <div className="bg-[var(--background-white-0)]">
-      <div className="mx-auto w-[1440px] py-[20px]">
-        <SiteHeader />
+      <div className="pointer-events-none fixed left-1/2 top-0 z-50 w-full -translate-x-1/2 sm:top-[32px] sm:w-[calc(100%-40px)] sm:max-w-[1400px]">
+        <div className="pointer-events-auto">
+          <SiteHeader mobileOverlay mobilePanel />
+        </div>
       </div>
-      <main className="mx-auto flex h-[720px] w-[1440px] items-center justify-center">
-        <h1 className="text-[64px] font-medium leading-[1] tracking-[-1.28px] text-[var(--text-strong-950)]">
-          About
-        </h1>
+      <main>
+        <AboutHeroSection />
+        <AboutStorySection />
+        <AboutDoctorsSection />
+        <AboutFounderSection />
+        <WhyChooseUs />
+        <AboutGallerySection />
       </main>
       <SiteFooter />
     </div>
